186860759284b7d5a77f08288a0c40eebbcf7659
TimManiquet <tim.maniquet@kuleuven.be>
2023-03-06 15:00:57 +0100

    update setup fun

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='occlusion',
-    version='0.1',
+    version='v0.1',
     description='Functions to occlude objects in images.',
     author='Tim Maniquet',
     author_email='timotheemaniquet@gmail.com',
